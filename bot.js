32389c4e6fd2e5e3e95ec3591037832c07f700ec
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-08-08 00:48:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,6 @@ const moment = require('moment');
 const ms = require("ms");
 var jimp = require('jimp')
 const arraySort = require('array-sort'),
-const child_process = require("child_process");
 const stripIndents = require('common-tags').stripIndents;
 const pretty = require('pretty-ms');
 const rn = require('random-number');
