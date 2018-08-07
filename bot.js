4216d0ab78a10b66b9e0044ba941ad561d1819db
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-08-07 22:51:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 var fs = require("fs");
 const arraySort = require('array-sort'),
-let userData = JSON.parse(fs.readFileSync(`./antilinks.json`, `utf8`));
 const ytdl = require("ytdl-core");
 const id = JSON.parse(fs.readFileSync("./id/mozo.json", "utf8"));
 let profile = JSON.parse(fs.readFileSync("./profile.json", "utf8"))
