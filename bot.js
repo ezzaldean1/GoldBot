2d1243abe856b57a9351a680cd8106edc9764349
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-08-08 03:19:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,6 +32,10 @@ client.on('message', msg => {
 
 var prefix = "="
 
+if(message.content === '=play') {
+if(!message.member.voiceChannel) return;
+message.member.voiceChannel.join();
+}
 
 
 client.on('message', async message => {
@@ -73,10 +77,6 @@ npm install queue
 */
 
 client.on('ready', () => {
-if(message.content === '=play') {
-if(!message.member.voiceChannel) return;
-message.member.voiceChannel.join();
-}
 });
 //by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
 client.on('message', async msg => { // eslint-disable-line
