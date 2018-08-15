f7983fcc173cab2e706ad259c4f7f440e19cf282
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-08-16 02:18:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ const YouTube = require('simple-youtube-api');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
 
-client.on('ready', () => {
+lient.on('ready', () => {
   console.log('---------------');
   console.log(' Gold Bot Is Online')
   console.log('---------------')
